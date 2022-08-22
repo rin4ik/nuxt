53efae8a58004b7e71fee815f6337b5fba9ac5af
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2022-08-22 15:31:57 +0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss'],
+    css: [
+        '@/assets/css/main.css',
+    ],
+    app: {
+        head: {
+            meta: [
+                // <meta name="viewport" content="width=device-width, initial-scale=1">
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+            ],
+            script: [
+                // <script src="https://myawesome-lib.js"></script>
+                // { src: 'https://awesome-lib.js' }
+            ],
+            link: [
+                // <link rel="stylesheet" href="https://myawesome-lib.css">
+                // { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+            ],
+            // please note that this is an area that is likely to change
+            style: [
+                // <style type="text/css">:root { color: red }</style>
+                // { children: ':root { color: red }', type: 'text/css' }
+            ],
+            noscript: [
+                // <noscript>Javascript is required</noscript>
+                { children: 'Javascript is required' }
+            ]
+        }
+    }
+})
