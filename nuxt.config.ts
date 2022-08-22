a486c3fe023ed45cc66c3d4a188a29175cb44e34
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2022-08-22 16:27:10 +0500

    add sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     css: [
-        '@/assets/css/main.css',
+        '@/assets/app.scss',
     ],
     app: {
         head: {
