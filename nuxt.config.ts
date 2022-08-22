f6acd2a8c6457734dc620878cf624618d193b315
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2022-08-22 17:23:09 +0500

    add extra libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
+    build: {
+        transpile: ['@heroicons/vue']
+    },
     css: [
         '@/assets/app.scss',
     ],
