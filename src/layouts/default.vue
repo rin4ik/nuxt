513964e73ae27b3b4f01cab6ec7c844092bb3d53
rin4ik <m.mirakhmad@gmail.com>
2022-08-23 16:15:25 +0500

    add default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-full">
+        <Navbar/>
+        <main class="mt-16 mx-auto max-w-6xl px-4 sm:mt-24">
+            <NuxtPage/>
+        </main>
+    </div>
+</template>
+<script lang="ts">
+export default defineComponent({
+    
+})
+</script>
